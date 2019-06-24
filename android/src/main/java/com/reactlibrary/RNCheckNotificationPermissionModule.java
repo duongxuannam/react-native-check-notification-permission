cf28dc45e380393fc0f6849541a6c6b5e95e5181
Xuân Nam Dương <duongxuannam1995@gmail.com>
2019-06-24 16:03:23 +0700

    fix-bug-android

--- android/src/main/java/com/reactlibrary/RNCheckNotificationPermissionModule.java
+++ android/src/main/java/com/reactlibrary/RNCheckNotificationPermissionModule.java
@@ -1,10 +1,14 @@
 
 package com.reactlibrary;
 
+import android.widget.Toast;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 
 public class RNCheckNotificationPermissionModule extends ReactContextBaseJavaModule {
 
